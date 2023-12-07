33333a1881e4a5ceb10829fba527622272a40359
SongDaeYoung <129142580+DaeYoung1@users.noreply.github.com>
2023-12-07 17:46:55 +0900

    six commit

---
+++ 3-3/3-3.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define ROWS 3
+#define COLS 3
+
+void main(void) {
+	int A[ROWS][COLS] = { {1,0,0} , {0,1,0}, {0,0,1} };
+	int B[ROWS][COLS] = { {1,0,0} , {0,1,0}, {0,0,1} };
+	int C[ROWS][COLS];
+
+	//두개행렬더한다.
+	for (int i = 0; i < ROWS; i++) {
+		for (int j = 0; j < COLS; j++) {
+			C[i][j] = A[i][j] + B[i][j];
+			printf("%2d", C[i][j]);
+		}
+		printf("\n");
+	}
+	
+}
